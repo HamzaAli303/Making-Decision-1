8aae2581903bb7373e5cb6b4bd22f98cd1910423
HamzaAli303 <129790190+HamzaAli303@users.noreply.github.com>
2023-04-04 15:44:52 -0600

    maxOfTwo

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+
+if (y > x) {
+    console.log(2)
+} else if (x > y) {
+    console.log(1);
+} else {
+    console.log('They are the same!')
+}
